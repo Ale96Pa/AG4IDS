f9771e594f7c13afd4d06379a788ae7a22775cef
Alessandro Palma <palma.1871556@studenti.uniroma1.it>
2025-06-10 12:22:30 +0200

    alert-based AG

--- buildIDSnet.py
+++ buildIDSnet.py
@@ -1,5 +1,6 @@
 import json, os, requests, time
 import networkx as nx
+import pandas as pd
 
 URL_CVE = "https://services.nvd.nist.gov/rest/json/cves/2.0?"
 SERVICES={
@@ -39,6 +40,52 @@ def get_dump_nvd(vulnFile):
         outfile.write(json_data)
     
     # return dump_cve
+
+def get_dump_cveList(ruleFolder, vulnFile):
+    dictAttackCve = {}
+    for ruleFile in os.listdir(ruleFolder):
+        with open(ruleFolder+ruleFile, 'r', encoding='utf-8', errors='ignore') as f:
+            if "ftp" in ruleFile: attackType = "ftp"
+            elif "dos" in ruleFile: attackType = "dos"
+            dictAttackCve[attackType] = []
+            
+            for line in f:
+                if "cve" in line:
+                    for field in line.split(";"):
+                        if "cve" in field:
+                            if "reference" in field:
+                                cveID = field.split(":")[1].replace("cve,","CVE-")
+                                if "CVE-CVE-" in cveID: cveID.replace("CVE-CVE-","CVE-")
+                                if "CVE-" in cveID and cveID not in dictAttackCve[attackType]:
+                                    dictAttackCve[attackType].append(cveID)
+                            elif "metadata" in field:
+                                for subfield in field.split(","):
+                                    if "cve" in subfield: 
+                                        cveID = subfield.split("cve ")[1].replace("_","-")
+                                        if "CVE-CVE-" in cveID: cveID.replace("CVE-CVE-","CVE-")
+                                        if "CVE-" in cveID and cveID not in dictAttackCve[attackType]: 
+                                            dictAttackCve[attackType].append(cveID)
+    
+    dump_cve={}
+    headers = {'content-type': 'application/json'}
+    for attack in dictAttackCve.keys():
+        dump_cve[attack] = []
+        for cve in dictAttackCve[attack]:
+            params={
+                "cveId": cve,
+            }
+            time.sleep(6)
+            response = requests.get(URL_CVE, params=params,  headers=headers)
+            if response.status_code == 200:
+                jsonResponse = response.json()
+                for cve in jsonResponse["vulnerabilities"]:
+                    dump_cve[attack].append(cve["cve"])
+    
+    with open(vulnFile, "w") as outfile:
+        json_data = json.dumps({"attacks":dump_cve}, 
+            default=lambda o: o.__dict__, indent=2)
+        outfile.write(json_data)
+
 
 def generate_devices(network_file,vulnerabilityFile):
     all_devs=[]
@@ -194,10 +241,116 @@ def getVulnsByService(servicename, vulnfile):
     if len(vulnsID)>7: return random.sample(vulnsID, 7), services[servicename]
     else: return vulnsID, services[servicename]
 
+def getVulnsByAlert(alertFolder, vulnFile, netFile, newNetFile):
+    dictDevAttack = {}
+    allAttacks = []
+    
+    for alertFile in os.listdir(alertFolder):
+        print(alertFile)
+        
+        with open(alertFolder+alertFile, 'r', encoding='utf-8', errors='ignore') as f: df = pd.read_csv(f, low_memory=False)
+        for index, row in df.iterrows():
+            ipAddr = row[' Destination IP']
+            attackType = row[' Label']
+            if type(attackType) != str: continue
+            
+            if "DoS" in attackType: attackType="dos"
+            if "DDoS" in attackType: attackType="dos"
+            if "FTP" in attackType: attackType="ftp"
+            if ipAddr=="172.16.0.1": ipAddr="205.174.165.80"
+            
+            if attackType == "BENIGN": continue
+            if attackType == "NaN": continue
+            if ipAddr == "NaN": continue
+            if attackType not in ["DoS","FTP","dos","ftp"]: continue
+            
+            if ipAddr not in dictDevAttack.keys(): dictDevAttack[ipAddr] = [attackType]
+            else: 
+                if attackType not in dictDevAttack[ipAddr]: dictDevAttack[ipAddr].append(attackType)
+            
+            if attackType not in allAttacks: allAttacks.append(attackType)
+    
+    print(set(allAttacks))
+    print(dictDevAttack)
+    
+    with open(vulnFile) as vulnf: attacks = json.load(vulnf)["attacks"]
+    with open(netFile) as netf: content = json.load(netf)
+    
+    devices = content["devices"]
+    vulnerabilities = content["vulnerabilities"]
+    edges = content["edges"]
+    
+    all_devs=[]
+    added_vulns=[]
+    for dev in devices:
+        ifaceObjs=[]
+        for iface in dev["network_interfaces"]:
+            ip = iface["ipaddress"]
+            cveAttackList=[]
+            if ip in dictDevAttack.keys():
+                for atck in dictDevAttack[ip]:
+                    for cve in attacks[atck]:
+                        if cve["id"] not in cveAttackList:
+                            cveAttackList.append(cve["id"])
+                            added_vulns.append(cve)
+            for port in iface["ports"]:
+                for srv in port["services"]:
+                    oldCve = srv["cve_list"]
+                    allCVE = oldCve+cveAttackList
+            
+            ifaceObjs.append({
+                "ipaddress": ip,
+                "macaddress": "ff:ff:ff:ff:ff:ff",
+                "ports": [
+                    {
+                        "number": 0,
+                        "state": "open",
+                        "protocol": "TCP",
+                        "services": [
+                            {
+                                "name": "all",
+                                "cpe_list": [],
+                                "cve_list": allCVE
+                            }
+                        ]
+                    }
+                ]
+                }
+            )
+        
+        all_devs.append(
+                {
+                    "id": dev["id"],
+                    "hostname": dev["hostname"],
+                    "type": dev["type"],
+                    "network_interfaces": ifaceObjs,
+                    "local_applications": []
+                }     
+        )
+    
+    with open(newNetFile, "w") as outfile:
+        json_data = json.dumps({"devices":all_devs,
+                                "vulnerabilities":vulnerabilities+added_vulns,
+                                "edges":edges}, 
+            default=lambda o: o.__dict__, indent=2)
+        outfile.write(json_data)
+    
 
 if __name__=="__main__":
-    vulnerabilityFile = "data/vulns.json"
     network_file = "data/CiC17Net.json"
+    alertNetworkFile = "data/CiC17NetAlert.json"
+    
+    """First generation of network inventory from CIC-IDS"""
+    # vulnerabilityFile = "data/vulns.json"
     # get_dump_nvd(vulnerabilityFile)
     # listCve = getVulnsByService("ubu16")
-    generate_devices(network_file,vulnerabilityFile)
+    # generate_devices(network_file,vulnerabilityFile)
+    
+    """Retrieving vulnerabilities from the rule dataset"""
+    # rule_folder = "emerging_rules/"
+    # get_dump_cveList(rule_folder, "vulnsAttack.json")
+    
+    """Build the alert-based network inventory"""
+    vulnAttackFile = "data/vulnsAttack.json"
+    alert_folder = "data/TrafficLabelling/"
+    getVulnsByAlert(alert_folder, vulnAttackFile, network_file, alertNetworkFile)
