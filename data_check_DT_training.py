972910397f2942f87954a85d6e19c6ca1cb04af6
Enkeleda Bardhi <50584293+bardhienkeleda@users.noreply.github.com>
2025-06-12 18:01:02 +0200

    feat: initial data cleaning

---
+++ data_check_DT_training.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+def flow_selector_ips(df, source_ip, destination_ip):
+    # if df[' Source IP'] == source_ip:
+    #     print("Source IP row found!")
+    # else:
+    #     print("Source IP row not found!")
+    print(df[' Source IP'][0])
+    return df.loc[(df[' Source IP'] == source_ip) & (df[' Destination IP'] == destination_ip)]#.style.hide_index().hide_columns()
+
+def flow_selector_attack(df, attack_class):
+    # if df[' Source IP'] == source_ip:
+    #     print("Source IP row found!")
+    # else:
+    #     print("Source IP row not found!")
+    print(df[' Source IP'][0])
+    return df.loc[df[' Label'] == attack_class]#.style.hide_index().hide_columns()
+
+def unique_data_creation(mon_data, tue_data, wed_data, fri_data):
+    '''
+    This function creates a unique file with all the data.
+    '''
+    data_list = [mon_data, tue_data, wed_data, fri_data]
+    data_list_dict = {0:'Monday data', 1:'Tuesday data', 2:'Wednesday data', 3:'Friday data'}
+
+    print("Seperate datasets dimensions: ")
+    for i, data in enumerate(data_list):
+        rows, cols = data.shape
+        print(f"{data_list_dict.get(i)} data has {rows} rows and {cols} columns")
+
+    data = pd.concat(data_list)
+    rows, cols = data.shape
+    print("Full dataset dimension:")
+    print(f"The full dataset has {rows} rows and {cols} columns")
+
+    # Check for duplicates
+    duplicates =  data[data.duplicated()]
+    print(f"The number of duplicates is {len(duplicates)}")
+
+    data.drop_duplicates(inplace=True)
+    rows, cols = data.shape
+    print("Full dataset dimension:")
+    print(f"The full dataset after dropping duplicates has {rows} rows and {cols} columns")
+
+    # Check for missing and infinite values
+    missing_values = data.isna().sum()
+    print("Missing values: ")
+    print(missing_values.loc[missing_values > 0])
+    numeric_columns = data.select_dtypes(include=np.number).columns
+    infinite_values = np.isinf(data[numeric_columns]).sum()
+    print("Infinite values: ")
+    print(infinite_values[infinite_values > 0])
+
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    print("New missing values: ")
+    missing_values = data.isna().sum()
+    print(missing_values.loc[missing_values > 0])
+
+    # Filling missing values with median
+    med_flow_bytes = data['Flow Bytes/s'].median()
+    med_flow_packets = data[' Flow Packets/s'].median()
+    data['Flow Bytes/s'] = data['Flow Bytes/s'].fillna(med_flow_bytes)
+    data[' Flow Packets/s'] = data[' Flow Packets/s'].fillna(med_flow_packets)
+
+    print(data[' Label'].unique())
+    print(data[' Label'].value_counts())
+
+    # Remove whitespace before column names
+    data = data.rename(columns=lambda x: x.strip())
+    return data 
+
+def clean_data_with_scenarios(scenario, full_data):
+
+    if scenario=="benign_dos_ftp":
+        label_list = ['BENIGN', 'DoS Hulk', 'DoS GoldenEye', 'DoS slowloris', 'DoS Slowhttptest', 'FTP-Patator']
+    else:
+        label_list = ['BENIGN', 'DoS Hulk', 'DoS GoldenEye', 'DoS slowloris', 'DoS Slowhttptest', 'FTP-Patator', 'PortScan']
+    
+    selected_data = full_data[full_data['Label'].isin(label_list)]
+    rows, cols = selected_data.shape
+    print("Selected dataset dimension:")
+    print(f"The selected dataset has {rows} rows and {cols} columns with these labels {selected_data['Label'].unique()}")
+
+
+if __name__ == "__main__":
+
+    mon_data = pd.read_csv("data/Monday-WorkingHours.pcap_ISCX.csv")
+    tue_data = pd.read_csv("data/Tuesday-WorkingHours.pcap_ISCX.csv")
+    wed_data = pd.read_csv("data/Wednesday-WorkingHours.pcap_ISCX.csv")
+    fri_data = pd.read_csv("data/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv")
+
+    # flow_selector_ips(tue_data, '205.174.165.73', '205.174.165.68')
+    # print(flow_selector_ips(wed_data, '205.174.165.73', '192.168.10.50'))
+    # print(flow_selector_attack(wed_data, 'DoS Hulk'))
+
+    full_data = unique_data_creation(mon_data, tue_data, wed_data, fri_data)
+    clean_data_with_scenarios("benign_dos_ftp", full_data)
+
+ 
